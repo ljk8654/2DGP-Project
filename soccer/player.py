6996460c50162fbb62825766c799de3a7c6b224e
ljk8654 <132908803+ljk8654@users.noreply.github.com>
2023-10-28 21:20:38 +0900

    2주차-1 :플레이어 리소스 바꿈, 좌우상하 움직임 추가

---
+++ soccer/player.py
@@ -0,0 +1,145 @@
+from pico2d import *
+import math
+
+
+# define event check fuctions
+
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+def up_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_UP
+
+
+def up_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_UP
+
+
+def down_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_DOWN
+
+
+def down_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_DOWN
+
+
+
+
+class Move:
+    @staticmethod
+    def enter(player, e):
+        if right_down(e) or left_up(e):
+            player.x_dir, player.action, player.y_dir = 1, 1, 0
+        elif left_down(e) or right_up(e):
+            player.x_dir, player.action, player.y_dir = -1, 3, 0
+        elif up_down(e) or up_up(e):
+            player.y_dir, player.action, player.x_dir = 1, 2, 0
+        elif down_down(e) or down_up(e):
+            player.y_dir, player.action, player.x_dir = -1, 0, 0
+
+    @staticmethod
+    def exit(player, e):
+        pass
+
+    @staticmethod
+    def do(player):
+        player.frame = (player.frame + 1) % 4
+        player.x += player.x_dir * 5
+        player.y += player.y_dir * 5
+        pass
+
+    @staticmethod
+    def draw(player):
+        if player.action == 3:
+
+        player.image.clip_draw(player.frame * 40, player.action * 43, 40, 43, player.x, player.y)
+        
+
+
+class Idle:
+    @staticmethod
+    def enter(player, e):
+        if player.action == 0:
+            player.action = 0
+        elif player.action == 1:
+            player.action = 1
+
+    @staticmethod
+    def exit(player, e):
+        pass
+
+    @staticmethod
+    def do(player):
+        player.frame = (player.frame + 1) % 4
+        pass
+
+    @staticmethod
+    def draw(player):
+        player.image.clip_draw(player.frame * 40, player.action * 43, 40, 43,
+                            player.x, player.y)
+        pass
+
+
+class StateMachine:
+    def __init__(self, player):
+        self.cur_state = Idle
+        self.player = player
+        self.transitions = {
+            Idle: {right_down: Move, right_up: Move, left_down: Move, left_up: Move, down_down: Move, down_up: Move, up_down: Move, up_up: Move},
+            Move: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, down_down: Idle, up_down: Idle, up_up: Idle, down_up: Idle}
+        }
+
+    def start(self):
+        self.cur_state.enter(self.player, ('NONE', 0))
+
+    def handle_event(self, e):
+        for check_event, next_state in self.transitions[self.cur_state].items():
+            if check_event(e):
+                self.cur_state.exit(self.player, e)
+                self.cur_state = next_state
+                self.cur_state.enter(self.player, e)
+                return True
+        return False
+
+    def update(self):
+        self.cur_state.do(self.player)
+        pass
+
+    def draw(self):
+        self.cur_state.draw(self.player)
+
+
+class Player:
+    def __init__(self):
+        self.x, self.y = 400, 90
+        self.frame = 0
+        self.x_dir = 0
+        self.y_dir = 0
+        self.action = 1
+        self.image = load_image('soccer_character_red.png')
+        self.state_mashine = StateMachine(self)
+        self.state_mashine.start()
+
+    def update(self):
+        self.frame = (self.frame + 1) % 4
+        self.state_mashine.update()
+
+    def handle_event(self, event):
+        self.state_mashine.handle_event(('INPUT', event))
+        pass
+
+    def draw(self):
+        self.state_mashine.draw()
---
+++ soccer/soccer.py
@@ -0,0 +1,59 @@
+from pico2d import *
+
+from player import Player
+
+
+# Game object class here
+
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            player.handle_event(event)
+
+
+def reset_world():
+    global running
+    global grass
+    global team
+    global world
+    global player
+
+    running = True
+    world = []
+
+    player = Player()
+    world.append(player)
+
+
+
+def update_world():
+    for o in world:
+        o.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+
+
+open_canvas()
+reset_world()
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
+# finalization code
+close_canvas()
