b2e9a6715db0b3dd4e3b5659753d0a0b45adffb2
ljk8654 <132908803+ljk8654@users.noreply.github.com>
2023-11-23 20:00:54 +0900

    필드 회전 및 확대

--- soccer/field.py
+++ soccer/field.py
@@ -2,16 +2,12 @@ from pico2d import *
 import soccer
 class Field:
     def __init__(self):
-        self.y = 450
         self.image = load_image('field.png')
 
     def update(self):
-        self.y = 450 - soccer.ball.y
-        if self.y < 100:
-            self.y = 100
         pass
 
     def draw(self):
-        self.image.draw(0, self.y)
+        self.image.draw(370, 670)
 
 
