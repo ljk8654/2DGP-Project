6d0bfc38ed92e28abd123e79511b2e7da9e3a88a
ljk8654 <132908803+ljk8654@users.noreply.github.com>
2023-11-28 23:46:24 +0900

    픽셀당 속도와 공과 사람 속도 재조정

--- soccer/player.py
+++ soccer/player.py
@@ -1,14 +1,12 @@
 from pico2d import *
 import game_framework
-# zombie Run Speed
-PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
-RUN_SPEED_KMPH = 30.0  # Km / Hour
+PIXEL_PER_METER = (1480.0 / 10)  # 10 pixel 30 cm
+RUN_SPEED_KMPH = 10.0  # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 import soccer
 
-# zombie Action Speed
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 10.0
@@ -299,7 +297,8 @@ class StateMachine:
         if space_down(e) and soccer.ball.dribble_state == 1:
             soccer.ball.shoot = 1
             soccer.ball.dribble_state = 2
-
+            soccer.ball.x_dir = self.player.x_dir
+            soccer.ball.y_dir = self.player.y_dir
         for check_event, next_state in self.transitions[self.cur_state].items():
             if check_event(e):
                 self.cur_state.exit(self.player, e)
--- soccer/soccer_ball.py
+++ soccer/soccer_ball.py
@@ -2,8 +2,8 @@ from pico2d import *
 import world
 import soccer
 import game_framework
-PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
-RUN_SPEED_KMPH = 30.0  # Km / Hour
+PIXEL_PER_METER = (1480 / 10)  # 1480 pixel 100 m
+RUN_SPEED_KMPH = 100.0  # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
@@ -56,24 +56,27 @@ class Ball:
         Ball.image = load_image('ball21x21.png')
         self.dribble_state = 0
         self.shoot = 0
-        self.shoot_range = 100
-
+        self.shoot_range = 400
+        self.x_dir,self.y_dir = 0, 0
     def update(self):
 
         if self.dribble_state != 1 and self.shoot == 1 and self.shoot_range > 0:
             self.shoot_range -= 10
-            self.x += soccer.player.x_dir * 10
-            self.y += soccer.player.y_dir * 10
+            self.x += self.x_dir * RUN_SPEED_PPS * game_framework.frame_time
+            self.y += self.y_dir * 5
+            if self.y > self.bg.h -150 or self.y < 150:
+                self.y_dir *= -1
         elif self.dribble_state == 1:
             self.x = soccer.player.x + 10 * soccer.player.x_dir
             self.y = soccer.player.y + 10 * soccer.player.y_dir
 
+
         if self.shoot_range == 0:
-            self.shoot_range = 300
+            self.shoot_range = 400
             self.dribble_state = 0
             self.shoot = 0
         self.x = clamp(110, self.x, self.bg.w - 110)
-        self.y = clamp(10, self.y, self.bg.h - 100)
+        self.y = clamp(140, self.y, self.bg.h - 100)
 
     def handle_event(self, event):
         pass
